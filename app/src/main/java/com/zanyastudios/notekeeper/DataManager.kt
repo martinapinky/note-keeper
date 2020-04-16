db54412fb48b5950cb5f2e3dfaa49773e93d1099
Martina Alinda <martinaalinda1@gmail.com>
2020-04-16 14:06:12 +0300

    Initial Commit

---
+++ app/src/main/java/com/zanyastudios/notekeeper/DataManager.kt
@@ -0,0 +1,24 @@
+package com.zanyastudios.notekeeper
+
+class DataManager {
+
+    val courses = HashMap<String, CourseInfo>()
+    val notes = ArrayList<NoteInfo>()
+
+    init {
+        initializeCourses()
+    }
+
+    private fun initializeCourses() {
+        var course = CourseInfo("android_intents", "Android Programming with intents")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo("android_async", "Android Async Programming and Services")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo(title = "Java Fundamentals: The Java Language", courseId = "java_lang")
+        courses.set(course.courseId, course)
+
+
+    }
+}
